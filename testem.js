d1511eb1815b7193faa23a47df6bec88c22641e1
Chris Watts <cwatts@simplymeasured.com>
2016-05-25 11:16:24 -0700

    Fix failing tests

--- testem.js
+++ testem.js
@@ -1,13 +1,32 @@
 /*jshint node:true*/
-module.exports = {
-  "framework": "qunit",
-  "test_page": "tests/index.html?hidepassed",
-  "disable_watching": true,
-  "launch_in_ci": [
-    "PhantomJS"
-  ],
-  "launch_in_dev": [
-    "PhantomJS",
-    "Chrome"
-  ]
-};
+'use strict';
+
+let testemConfig = {};
+
+if (process.env.CI) {
+  testemConfig = {
+    framework: 'qunit',
+    test_page: 'tests/index.html?hidepassed',
+    report_file: `${process.env.CIRCLE_TEST_REPORTS}/ember/report.xml`,
+    reporter: 'xunit',
+    disable_watching: true,
+    xunit_intermediate_output: true,
+    launch_in_ci: [
+      'Chrome'
+    ]
+  };
+} else {
+  testemConfig = {
+    framework: 'qunit',
+    test_page: 'tests/index.html?hidepassed&coverage',
+    disable_watching: true,
+    launch_in_ci: [
+      'Chrome'
+    ],
+    launch_in_dev: [
+      'Chrome'
+    ]
+  };
+}
+
+module.exports = testemConfig;
